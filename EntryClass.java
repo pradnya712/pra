7e3460d03531cb982a7a70d177cea97f49a2d4a1
pradnya shelke <pradnyashelke21@gmail.com>
2019-10-14 12:02:45 -0700

    Using buffererReader

--- EntryClass.java
+++ EntryClass.java
@@ -1,8 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class EntryClass{
 
-   /*  public void studentMethod(int id, String name, int stclass,Double precentage){
+     public void studentMethod(int id, String name, int stclass,Double precentage){
         Student student = new Student(id,name,stclass,precentage);
         System.out.println("Student ID: "+student.getId());
         System.out.println("Student name: "+student.getStName());
@@ -11,9 +14,9 @@ public class EntryClass{
         System.out.println("******"); 
 
     }
- */
+ 
 
-    /* public void employeeMethod(int id, String firstName, String lastName){
+     public void employeeMethod(int id, String firstName, String lastName){
         Employee employee = new Employee(id,firstName,lastName);
         System.out.println(" Employee ID: "+employee.getId());
         System.out.println("Employee firstName: "+employee.getFirstName());
@@ -21,11 +24,15 @@ public class EntryClass{
         System.out.println("******");
 
     }
-     */
+     
 
     public void studentInput(){
         Student student = new Student();
         Scanner sc = new Scanner(System.in);
+        
+        InputStreamReader r=new InputStreamReader(System.in); 
+        BufferedReader br=new BufferedReader(r); 
+        
         
         System.out.println("Enter Student Id: ");
         student.setId(sc.nextInt());
@@ -53,13 +60,14 @@ public class EntryClass{
         Employee employee = new Employee();
         Scanner sc1 = new Scanner(System.in);
 
+        InputStreamReader r1=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(r1); 
         System.out.println("Entry employee Id: ");
         employee.setId(sc1.nextInt());
         System.out.println("employee id: "+employee.getId());
 
         System.out.println("entry employee firstName: ");
-        employee.setFirstName(sc1.nextLine());
-        //System.out.println("employee firstName:"+employee.getFirstName());
+        employee.setFirstName(sc1.next());
         System.out.println("Employee First Name: "+employee.getFirstName());
 
         System.out.println("Entry employee Lastname: ");
---
+++ EntryClassBR.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParsePosition;
+
+
+public class EntryClassBR{
+
+   
+
+    public void studentInput(){
+        Student student = new Student();
+        //Scanner sc = new Scanner(System.in);
+       
+        try{
+       
+        InputStreamReader r=new InputStreamReader(System.in); 
+        BufferedReader br=new BufferedReader(r); 
+        
+        
+        System.out.println("Enter Student Id: ");
+        student.setId(Integer.parseInt(br.readLine()));
+        
+        System.out.println("Enter Student Name:");
+        student.setStName(br.readLine());
+        
+        System.out.println("Enter Student Class:");
+        student.setStclass(Integer.parseInt(br.readLine()));
+        
+        System.out.println("Enter Student Percentage:");
+        student.setPrecentage(Double.parseDouble(br.readLine()));
+        
+        System.out.println("**************************");
+        System.out.println("Student ID: "+student.getId());
+        System.out.println("student stName: "+student.getStName());
+        System.out.println("student stclass: "+student.getStclass());
+        System.out.println("student precentage: "+student.getPrecentage());
+        
+    }catch (IOException e){
+    System.out.println(e);
+}
+}
+        
+    
+        public void employeeInput(){
+        Employee employee = new Employee();
+       // Scanner sc1 = new Scanner(System.in);
+       
+       try{
+       
+        InputStreamReader r1=new InputStreamReader(System.in);
+        BufferedReader br1=new BufferedReader(r1); 
+       
+        System.out.println("Entry employee Id: ");
+        employee.setId(Integer.parseInt(br1.readLine()));
+        
+        System.out.println("entry employee firstName: ");
+        employee.setFirstName(br1.readLine());
+        
+        System.out.println("Entry employee Lastname: ");
+        employee.setLastName(br1.readLine());
+        
+        System.out.println("*****************************");
+        System.out.println("employee id: "+employee.getId());
+        System.out.println("Employee First Name: "+employee.getFirstName());
+        System.out.println("employee Lastname: "+employee.getLastName());
+
+    }
+    catch (IOException e){
+        System.out.println(e);
+    }
+    }
+    
+    public static void main(String[] args) {
+         
+        EntryClassBR entryClassbr = new EntryClassBR();
+        entryClassbr.studentInput();
+        entryClassbr.employeeInput();
+        
+    }
+
+}    
+
+    
+
