bf8d31cc45c44a5aea3071b048490f408af5b552
pradnya shelke <pradnyashelke21@gmail.com>
2019-10-14 13:55:40 -0700

    stringbuilder

---
+++ Readingfile.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Readingfile {
+
+    public static void main(String[]arg){
+        StringBuilder sb = new StringBuilder();
+       
+  try {
+    BufferedReader bReader = Files.newBufferedReader(Paths.get("studentname"));
+    
+    String line;
+    while((line=bReader.readLine())!=null)
+       sb.append(line).append("\n");
+
+
+   System.out.println(sb);
+  }
+   catch (Exception e) {
+    System.out.println(e);
+  }
+       
+}
+}
